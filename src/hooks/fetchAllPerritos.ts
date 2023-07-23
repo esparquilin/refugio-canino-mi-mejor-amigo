e1e525273b66efee4d5ef7dff4882f5411f9b939
Angel Chapulin <esparquilin@ciencias.unam.mx>
2023-07-23 12:43:34 -0600

    custom react hook to search all dogs

--- src/hooks/fetchAllPerritos.ts
+++ src/hooks/fetchAllPerritos.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import { allDogsData } from "../interfaces/dogInterfaces";
-
+//react hook
 const useFetchAllPerritos = (
   page: number,
   filterSize: String,
